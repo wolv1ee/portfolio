22d0c3ecaf3df18cf0f1dd839d7c8bc2c21123ee
Saad Mahmud <mdsaad111.s3@gmail.com>
2026-09-08 14:00:50 -0400

    Switch to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://saadmahmud.dev',
   integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
